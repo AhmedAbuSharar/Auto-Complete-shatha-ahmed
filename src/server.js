c7a6038fceff8b75dee87513a60b7cc4d3c299f7
Ahmed Abu Sharar <absam9945@gmail.com>
2022-08-13 01:07:55 +0300

    create a server in server file relates #10

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const router= require('./router')
+
+const server = http.createServer(router)
+
+const port=4000
+server.listen(port, ()=>{
+    console.log(`server is listening on port ${port}  ready to accept requests`);
+})
